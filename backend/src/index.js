e288d864e61c59612c5bc89d6e22c86c23c73e56
chrisWoodard74 <chriswoodard74@hotmail.com>
2026-09-14 17:07:06 -0400

    second commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,24 @@
 import express from "express";
-import "dotenv/config"
+import cors from "cors";
+import "dotenv/config";
 
+import { clerkMiddleware } from "@clerk/express";
+
+import User from "./models/user.model.js"
+import { connectDB } from "./lib/db.js";
 
 const app = express();
-const PORT = process.env.PORT
-app.listen(PORT, () => console.log("Server is up and running on port:",PORT))
+const PORT = process.env.PORT;
+const FRONTEND_URL = process.env.FRONTEND_URL;
+
+app.use(express.json());
+app.use(cors({origin:FRONTEND_URL,credentials:true}));
+app.use(clerkMiddleware());
+
+app.get("/health", (req, res) => {
+    res.status(200).json({ok:true});
+})
+app.listen(PORT, () => {
+    connectDB();
+    console.log("Server is up and running on PORT:",PORT)
+})
