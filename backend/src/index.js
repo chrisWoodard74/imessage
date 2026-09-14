f629eceb40738f0740990e0c0a4a18082a795ae3
chrisWoodard74 <chriswoodard74@hotmail.com>
2026-09-14 18:55:44 -0400

    dockerFile included

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,11 @@
 import express from "express";
 import cors from "cors";
+
 import "dotenv/config";
+
+import fs from "fs";
+import path from "path";
+
 
 import { clerkMiddleware } from "@clerk/express";
 
@@ -8,8 +13,12 @@ import User from "./models/user.model.js"
 import { connectDB } from "./lib/db.js";
 
 const app = express();
+
 const PORT = process.env.PORT;
 const FRONTEND_URL = process.env.FRONTEND_URL;
+
+const publicDir = path.join(process.cwd(), "public");
+
 
 app.use(express.json());
 app.use(cors({origin:FRONTEND_URL,credentials:true}));
@@ -17,7 +26,18 @@ app.use(clerkMiddleware());
 
 app.get("/health", (req, res) => {
     res.status(200).json({ok:true});
-})
+});
+
+// if the public directory exists, serve the static files
+// this is for production buil
+if(fs.existsSync(publicDir)){
+app.use(express.static(publicDir))
+
+app.get("/{*any}",(req,res,next) => {
+    res.sendFile(path.join(publicDir, "index.html"),(err)=> next(err));
+});
+}
+
 app.listen(PORT, () => {
     connectDB();
     console.log("Server is up and running on PORT:",PORT)
