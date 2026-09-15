bd0ba141b41a79772342018511501e32bca76bd3
chrisWoodard74 <chriswoodard74@hotmail.com>
2026-09-15 11:06:54 -0400

    add authentication middleware and routes for user verification

---
+++ backend/src/routes/auth.route.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { checkAuth } from "../controllers/auth.controller.js";
+
+
+const router = express.Router();
+
+// /api/check
+router.get("/check",protectRoute,checkAuth);
+
+export default router;
