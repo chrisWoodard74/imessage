07e5df064f02850dfa612e10e4120932802cf763
chrisWoodard74 <chriswoodard74@hotmail.com>
2026-09-14 20:04:39 -0400

    cron fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,6 +11,7 @@ import { clerkMiddleware } from "@clerk/express";
 
 import User from "./models/user.model.js"
 import { connectDB } from "./lib/db.js";
+import { job } from "./lib/cron.js"
 
 const app = express();
 
@@ -41,5 +42,6 @@ app.get("/{*any}",(req,res,next) => {
 app.listen(PORT, () => {
     connectDB();
     console.log("Server is up and running on PORT:",PORT)
-   if(process.env.NODE_ENV === "production")job.start();
+   if(process.env.NODE_ENV === "production")
+    job.start();
 });
