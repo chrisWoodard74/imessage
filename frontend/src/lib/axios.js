bd4fddce536d336b1086c5aa22f31fa4ed8c3d0f
chrisWoodard74 <chriswoodard74@hotmail.com>
2026-09-19 19:50:49 -0400

    feat: implement chat functionality with message handling, user presence indicators, and UI components

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "deleopment" ? "http://localhost:3000" : "/api",
-    withCredentials: true,
-})
+  baseURL: import.meta.env.DEV ? "http://localhost:3000" : "/api",
+  withCredentials: true,
+});
